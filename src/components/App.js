9df3fd41c9ae070be3d6c4772c177676c84397c5
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2025-03-04 13:32:16 +0530

    c4

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,8 @@ const App = () => {
         <h1>User Information</h1>
         <div className="output">
           Name - {name}
+        </div>
+        <div className="output">
           Email -{email}
         </div>
     </div>
